c9993a3b6de64497b79da935807204daf9096f52
pawisweta <sweta.p2507@gmail.com>
2023-04-22 19:32:40 +0530

    Changes in Project done

--- signin.js
+++ signin.js
@@ -1,14 +1,28 @@
 const continueButton = document.getElementById('click');
 
 // add a click event listener to the button
-continueButton.addEventListener('click', function() {
+continueButton.addEventListener('click', function(){
   const mobileNumber = document.getElementById('phone').value;
   if (mobileNumber.length !== 10 || isNaN(mobileNumber)) {
     alert('Please enter a valid mobile number');
     return;
   }
-  // redirect to the signup.html page
-  window.location.href = 'signup.html';
+  
+  const storedUserData = JSON.parse(localStorage.getItem('userData'));
+
+  for (let i = 0; i < storedUserData.length; i++) {
+    if (storedUserData[i].userMobile === mobileNumber) {
+      // User already exists, redirect to login page
+      window.location.href = "loginpage.html";
+      return;
+    }
+  }
+
+  // Store the mobile number in the local storage
+  localStorage.setItem('phoneNumber', mobileNumber);
+
+  // User is new, redirect to sign up page
+  window.location.href = "signup.html";
 });
 
 // get the "Continue with email" button element
